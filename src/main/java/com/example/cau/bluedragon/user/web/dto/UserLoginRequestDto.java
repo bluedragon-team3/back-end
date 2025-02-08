5c75b4b7ef5b077b264ad3bc4f4dbd4179837400
only4wxx <orchidbyw1@gmail.com>
2025-02-09 03:58:25 +0900

    Feat: request dto에 not blank 추가

--- src/main/java/com/example/cau/bluedragon/user/web/dto/UserLoginRequestDto.java
+++ src/main/java/com/example/cau/bluedragon/user/web/dto/UserLoginRequestDto.java
@@ -1,9 +1,13 @@
 package com.example.cau.bluedragon.user.web.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class UserLoginRequestDto {
+  @NotBlank
   String signId;
+
+  @NotBlank
   String password;
 }
